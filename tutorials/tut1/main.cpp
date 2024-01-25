7a53e89006a8b3ea14823ee5493bd854b45b04ea
Kaleb Redpath <kaleb.redpath@ontariotechu.net>
2024-01-25 11:00:37 -0500

    Tutorial 1

--- tutorials/tut1/main.cpp
+++ tutorials/tut1/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
+#include <numbers>
 
+struct Point {
+    double x;
+    double y;
+};
+
+// Write a program that takes two points from cin and prints the distance between them.
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Point p1, p2;
+    std::cout << "Enter x and y coordinates of the first point: ";
+    std::cin >> p1.x >> p1.y;
+    std::cout << "Enter x and y coordinates of the second point: ";
+    std::cin >> p2.x >> p2.y;
+
+    double distance = std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
+    std::cout << "The distance between the two points is " << distance << std::endl;
+
     return 0;
 }
